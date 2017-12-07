ad7001f534f2f66c2a539091f59d8e6dfaaa6246
Petra Alice Mikova <pmikova@redhat.com>
2017-12-07 17:42:05 +0100

    Dao queries fixed

--- src/main/java/com/redhat/thermostat/vm/decompiler/data/VmDecompilerDAOImpl.java
+++ src/main/java/com/redhat/thermostat/vm/decompiler/data/VmDecompilerDAOImpl.java
@@ -30,36 +30,34 @@ import org.apache.felix.scr.annotations.Service;
 @Component
 @Service(value = VmDecompilerDAO.class)
 public class VmDecompilerDAOImpl extends AbstractDao implements VmDecompilerDAO{
-     
-     
+
     static final Key<Integer> PORT = new Key<>("listenPort");
-    //static final Key<StoreJvmInfo> STORE_JVM_INFO = new Key<>("storeJvmInfo");
-    
+    static final Key<String[]> LOADED_CLASS_NAMES = new Key<>("loadedClasses");
+    //static final Key<String[]> LOADED_BYTES= new Key<>("loadedBytes");
 
     public static final Category<VmDecompilerStatus> VM_DECOMPILER_STATUS_CATEGORY = new Category<>(
             "vm-decompiler-status",
             VmDecompilerStatus.class,
-            Key.AGENT_ID, Key.VM_ID, Key.TIMESTAMP, PORT/*, STORE_JVM_INFO*/);
-    
+            Key.AGENT_ID, Key.VM_ID, Key.TIMESTAMP, PORT, LOADED_CLASS_NAMES);
+
     public static final String QUERY_VM_DECOMPILER_STATUS = "QUERY " + VM_DECOMPILER_STATUS_CATEGORY.getName()
             + " WHERE '" + Key.VM_ID.getName() + "' = ?s LIMIT 1";
 
     public static final String REPLACE_OR_ADD_STATUS_DESC = "REPLACE " + VM_DECOMPILER_STATUS_CATEGORY.getName()
             + " SET '" + Key.AGENT_ID.getName() + "' = ?s , "
             + "'" + Key.VM_ID.getName() + "' = ?s , "
-            + "'" + Key.TIMESTAMP.getName() + "' = ?l , " 
-            + "'" + PORT.getName() + "' = ?i WHERE  "
-           /* + "'" + STORE_JVM_INFO + "' = ?p WHERE "*/
+            + "'" + Key.TIMESTAMP.getName() + "' = ?l , "
+            + "'" + PORT.getName() + "' = ?i , "
+            + "'" + LOADED_CLASS_NAMES + "' = ?s[ WHERE "
             + "'" + Key.VM_ID.getName() + "' = ?s";
 
-    
     @Reference
     private Storage storage;
-
 
     public VmDecompilerDAOImpl() {
         // Default constructor for DS
     }
+
     @Activate
     private void activate() {
         storage.registerCategory(VM_DECOMPILER_STATUS_CATEGORY);
@@ -68,13 +66,13 @@ public class VmDecompilerDAOImpl extends AbstractDao implements VmDecompilerDAO{
     public VmDecompilerDAOImpl(Storage storage) {
         this.storage = storage;
 
-    }      
-    
+    }
+
     @Override
     protected Logger getLogger() {
         return LoggingUtils.getLogger(VmDecompilerDAOImpl.class);
     }
-    
+
     @Override
     public VmDecompilerStatus getVmDecompilerStatus(final VmId vmId) {
         List<VmDecompilerStatus> result = executeQuery(new AbstractDaoQuery<VmDecompilerStatus>(storage, VM_DECOMPILER_STATUS_CATEGORY, QUERY_VM_DECOMPILER_STATUS) {
@@ -91,8 +89,6 @@ public class VmDecompilerDAOImpl extends AbstractDao implements VmDecompilerDAO{
         return result.get(0);
     }
 
-
-    
     @Override
     public void addOrReplaceVmDecompilerStatus(final VmDecompilerStatus status) {
         executeStatement(new AbstractDaoStatement<VmDecompilerStatus>(storage, VM_DECOMPILER_STATUS_CATEGORY, REPLACE_OR_ADD_STATUS_DESC) {
@@ -101,13 +97,14 @@ public class VmDecompilerDAOImpl extends AbstractDao implements VmDecompilerDAO{
             public PreparedStatement<VmDecompilerStatus> customize(PreparedStatement<VmDecompilerStatus> preparedStatement) {
                 preparedStatement.setString(0, status.getAgentId());
                 preparedStatement.setString(1, status.getVmId());
-                preparedStatement.setInt(2, status.getListenPort());
-                preparedStatement.setString(3, status.getVmId());
+                preparedStatement.setLong(2, status.getTimeStamp());
+                preparedStatement.setInt(3, status.getListenPort());
+                preparedStatement.setStringList(4, status.getClassNames());
+                preparedStatement.setString(5, status.getVmId());
                 return preparedStatement;
             }
         });
 
     }
-
 
 }
