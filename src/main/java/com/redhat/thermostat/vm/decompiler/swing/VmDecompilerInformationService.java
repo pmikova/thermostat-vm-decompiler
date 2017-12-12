fa117d46d981e3b6f91ae2475e007f711103a415
Petra Alice Mikova <pmikova@redhat.com>
2017-12-12 14:12:45 +0100

    Removed redundant lines

--- src/main/java/com/redhat/thermostat/vm/decompiler/swing/VmDecompilerInformationService.java
+++ src/main/java/com/redhat/thermostat/vm/decompiler/swing/VmDecompilerInformationService.java
@@ -34,31 +34,19 @@ import org.apache.felix.scr.annotations.Service;
     @Property(name = "GenericClassName",
               value = "com.redhat.thermostat.storage.core.VmRef"),
     @Property(name = "com.redhat.thermostat.client.core.InformationService.serviceID",
-              value = "com.redhat.thermostat.vm.decompiler.swing.VmDecompilerInformationService")
+            value = "com.redhat.thermostat.vm.decompiler.swing.VmDecompilerInformationService")
 })
 public class VmDecompilerInformationService implements InformationService<VmRef> {
     @Reference
     private AgentInfoDAO agentInfoDao;
-@Reference
+    @Reference
     private VmInfoDAO vmInfoDao;
-@Reference
+    @Reference
     private VmDecompilerDAO vmDecompilerDao;
-@Reference
+    @Reference
     private RequestQueue requestQueue;
 
     private Map<VmRef, VmDecompilerInformationController> controllers = new ConcurrentHashMap<>();
-    //private SwingVmDecompilerViewProvider provider;
-    //private ApplicationService service;
-/*
-    public VmDecompilerInformationService(ApplicationService service, AgentInfoDAO agentInfoDao, VmInfoDAO vmInfoDao,
-            RequestQueue queue, SwingVmDecompilerViewProvider provider
-            UIDefaults uiDefaults) {
-        this.service = service;
-        this.agentInfoDao = agentInfoDao;
-        this.vmInfoDao = vmInfoDao;
-        this.requestQueue = queue;
-        this.provider = provider;*/
-    
 
     @Override
     public int getOrderValue() {
