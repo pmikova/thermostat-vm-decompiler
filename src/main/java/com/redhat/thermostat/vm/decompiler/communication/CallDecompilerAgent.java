93e141a4392eb8cfa96da0c333968b09c3fb788f
Petra Alice Mikova <pmikova@redhat.com>
2017-12-16 14:12:16 +0100

    Added javadoc and logs

--- src/main/java/com/redhat/thermostat/vm/decompiler/communication/CallDecompilerAgent.java
+++ src/main/java/com/redhat/thermostat/vm/decompiler/communication/CallDecompilerAgent.java
@@ -1,58 +1,94 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2012-2017 Red Hat, Inc.
+ *
+ * This file is part of Thermostat.
+ *
+ * Thermostat is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2, or (at your
+ * option) any later version.
+ *
+ * Thermostat is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Thermostat; see the file COPYING.  If not see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Linking this code with other modules is making a combined work
+ * based on this code.  Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this code give
+ * you permission to link this code with independent modules to
+ * produce an executable, regardless of the license terms of these
+ * independent modules, and to copy and distribute the resulting
+ * executable under terms of your choice, provided that you also
+ * meet, for each linked independent module, the terms and conditions
+ * of the license of that module.  An independent module is a module
+ * which is not derived from or based on this code.  If you modify
+ * this code, you may extend this exception to your version of the
+ * library, but you are not obligated to do so.  If you do not wish
+ * to do so, delete this exception statement from your version.
  */
 package com.redhat.thermostat.vm.decompiler.communication;
 
-import java.io.PrintStream;
+import com.redhat.thermostat.common.utils.LoggingUtils;
+import com.redhat.thermostat.vm.decompiler.core.AgentAttachManager;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
- * @author pmikova
+ * This class is handling opening of communication socket and request submitting.
+ *  
  */
 public class CallDecompilerAgent {
-    
     
     public static final String DEFAULT_ADDRESS = "localhost";
     public static final int DEFAULT_PORT= 5395;
     
     private final int port;
     private final String address;
-
-    private PrintStream out;
+    private static final Logger logger = LoggingUtils.getLogger(CallDecompilerAgent.class);
     
-    public CallDecompilerAgent(int port) {
-        this(DEFAULT_ADDRESS, port, System.out);
-    }
-    
-    
-    public CallDecompilerAgent(String address, int port, PrintStream out) {
-        if (address == null) {
-            address = DEFAULT_ADDRESS;
+    /**
+     * Constructor of the object
+     * @param port port where to open socket
+     * @param host socket host
+     */
+    public CallDecompilerAgent(int port, String host) {
+        if (host == null) {
+            host = DEFAULT_ADDRESS;
         }
 
         if (port <= 0) {
             port = DEFAULT_PORT;
         }
 
-        if (out == null) {
-            out = System.out;
-        }
-
-        this.address = address;
+        this.address = host;
         this.port = port;
-        this.out = out;
+        logger.log(Level.FINEST, "Port assigned to: " + port + ", host: " + host);
     }
+
     
-    public String submitRequest(final String request) throws Exception {
+    /**
+     * Opens a socket and sends the request to the agent via socket. 
+     * @param request either "CLASSES" or "BYTES \n className", other formats
+     * are refused
+     * @return agents response or null
+     */
+    public String submitRequest(final String request){
         final Communicate comm = new Communicate(this.address, this.port);
         try {
             comm.println(request);
             String results = comm.readResponse();
             return results;
-        }catch(Exception ex){
-            ex.printStackTrace();
+        }catch(IOException ex){
+            logger.log(Level.SEVERE, "Communication with the agent failed,"
+                    + " could not send a request.");
             return null;
         }finally {
             comm.close();
--- src/main/java/com/redhat/thermostat/vm/decompiler/communication/Communicate.java
+++ src/main/java/com/redhat/thermostat/vm/decompiler/communication/Communicate.java
@@ -1,10 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2012-2017 Red Hat, Inc.
+ *
+ * This file is part of Thermostat.
+ *
+ * Thermostat is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2, or (at your
+ * option) any later version.
+ *
+ * Thermostat is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Thermostat; see the file COPYING.  If not see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Linking this code with other modules is making a combined work
+ * based on this code.  Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this code give
+ * you permission to link this code with independent modules to
+ * produce an executable, regardless of the license terms of these
+ * independent modules, and to copy and distribute the resulting
+ * executable under terms of your choice, provided that you also
+ * meet, for each linked independent module, the terms and conditions
+ * of the license of that module.  An independent module is a module
+ * which is not derived from or based on this code.  If you modify
+ * this code, you may extend this exception to your version of the
+ * library, but you are not obligated to do so.  If you do not wish
+ * to do so, delete this exception statement from your version.
  */
 package com.redhat.thermostat.vm.decompiler.communication;
 
+import com.redhat.thermostat.common.utils.LoggingUtils;
+import com.redhat.thermostat.vm.decompiler.core.AgentAttachManager;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -12,51 +44,66 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- *
- * @author pmikova
+ * This class opens a socket and contain methods for read and write to socket
+ * IS/OS.
  */
 public class Communicate {
 
     private Socket commSocket;
     private BufferedReader commInput;
     private BufferedWriter commOutput;
+
     public static final String DEFAULT_ADDRESS = "localhost";
+    private static final Logger logger = LoggingUtils.getLogger(Communicate.class);
 
-    public Communicate(int port) throws Exception{
-        this(DEFAULT_ADDRESS, port);
-    }
+    /**
+     * Constructor creates a socket on given port and saves the streams into
+     * class variables.
+     *
+     * @param host host name
+     * @param port port where we open the socket
+     */
+    public Communicate(String host, int port) {
 
-    public Communicate(String address, int port) throws Exception {
-
-        this.commSocket = new Socket(address, port);
+        try {
+            this.commSocket = new Socket(host, port);
+        } catch (IOException ex) {
+            Logger.getLogger(Communicate.class.getName()).log(Level.SEVERE, null, ex);
+        }
         InputStream is;
         try {
             is = this.commSocket.getInputStream();
-        } catch (Exception e) {
-            // oops. cannot handle this
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Opening of input stream of a socket "
+                    + "failed: " + e.getMessage());
             try {
                 this.commSocket.close();
-            } catch (Exception e1) {
-                e1.printStackTrace();
-                
+            } catch (IOException e1) {
+                logger.log(Level.WARNING, "Error while closing the socket: "
+                        + e1.getMessage());
+
             }
-            throw e;
+            return;
         }
 
         OutputStream os;
         try {
             os = this.commSocket.getOutputStream();
-        } catch (Exception e) {
-            // oops. cannot handle this
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Opening of output stream of a socket "
+                    + "failed: " + e.getMessage());
             try {
                 this.commSocket.close();
-            } catch (Exception e1) {
+            } catch (IOException e1) {
+                logger.log(Level.WARNING, "Error while closing the socket: "
+                        + "" + e1.getMessage());
             }
-            throw e;
+            return;
         }
 
         this.commInput = new BufferedReader(new InputStreamReader(is));
@@ -65,56 +112,87 @@ public class Communicate {
         return;
     }
 
+    /**
+     * Closes a socket.
+     */
     public void close() {
         try {
             this.commSocket.close(); // also closes the in/out streams
-        } catch (Exception e) {
-            e.printStackTrace();
-            // TODO what should I do here? no need to abort, we are closing this object anyway
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Error while closing the socket: "
+                    + "" + e.getMessage());
+
         } finally {
-            // this object cannot be reused anymore, therefore, null everything out
-            // which will force NPEs if attempts to reuse this object occur later
-            //this.commSocket = null;
+            this.commSocket = null;
             this.commInput = null;
             this.commOutput = null;
         }
     }
 
-    public String readResponse() throws Exception {
-        String initLine = this.commInput.readLine().trim();
-        if (initLine.equals("ERROR")) {
-            //error log
+    /**
+     * Method that reads agent's response.
+     * @return "ERROR" in case of fail or corresponding bytes or class names
+     */
+    public String readResponse(){
+        String initLine;
+        try {
+            initLine = this.commInput.readLine().trim();
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Agent did not return anything.");
             return "ERROR";
-        } else if (initLine.equals("BYTES")) {
-            String s = this.commInput.readLine();
-            s = s.trim();
-            return s;
-        } else if (initLine.equals("CLASSES")) {
+        }
+        
+        if (initLine.equals("ERROR")) {
+            logger.log(Level.SEVERE, "Agent returned error.");
+            return "ERROR";
+        } 
+        
+        else if (initLine.equals("BYTES")) {
+            try {
+                String s = this.commInput.readLine();
+                s = s.trim();
+                logger.log(Level.FINE, "Agent returned bytes.");
+                return s;
+            } catch (IOException ex) {
+                logger.log(Level.WARNING, "Can not read line, check "
+                        + "agent communication output.");
+            }
+
+        } 
+        
+        else if (initLine.equals("CLASSES")) {
             StringBuilder str = new StringBuilder();
             while (true) {
-                String s = this.commInput.readLine();
-                if (s == null) {
-                    break;
-                }
-                s = s.trim();
-                if (!s.isEmpty()) {
-                    str.append(s).append(";");
+                try {
+                    String s = this.commInput.readLine();
+                    if (s == null) {
+                        break;
+                    }
+                    s = s.trim();
+                    if (!s.isEmpty()) {
+                        str.append(s).append(";");
+                    }
+                } catch (IOException ex) {
+                    logger.log(Level.WARNING, "Can not read line, check "
+                        + "agent communication output.");
                 }
             }
+            logger.log(Level.FINE, "Agent returned class names.");
             return str.toString();
-        } else {
-            throw new RuntimeException("Unknow header of " + initLine);
-        }
+        } 
+        
+        logger.log(Level.SEVERE, "Unknow header of " + initLine);
+        return "ERROR";
     }
 
+    /**
+     * Sends a line with request to agent.
+     * @param line "CLASSES" or "BYTES className"
+     * @throws IOException if the write operation fails
+     */
     public void println(String line) throws IOException {
         this.commOutput.write(line);
         this.commOutput.newLine();
-        this.commOutput.flush();
-    }
-
-    public void print(String line) throws IOException {
-        this.commOutput.write(line);
         this.commOutput.flush();
     }
 
